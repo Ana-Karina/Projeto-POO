77bb71d476b609f701dd22fd8243361733ee28a8
Ana Karina Carvalho <31571011+Ana-Karina@users.noreply.github.com>
2018-08-25 19:00:37 -0300

    Projeto-POO

---
+++ Dados.java
@@ -0,0 +1,15 @@
+
+import java.util.Random;
+
+public class Dados {
+
+    private int valor;
+    private Random R;
+
+    public Dados() {
+        this.valor = 0;
+        this.R = new Random();
+
+
+    }
+}
